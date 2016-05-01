7ec0dff76bde403e059a8f23fd952d6a905404fa
Roman Tsukanov <roman@tsukanov.me>
2016-05-01 05:26:36 +0200

    Fix setup script to include missing parts

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
+from pytools import __version__
 
 setup(
     name="pytools",
-    version="1.0.0",
+    version=__version__,
     description="Python tools for MetaBrainz projects",
     author="MetaBrainz Foundation",
     author_email="support@metabrainz.org",
     py_modules=["pytools"],
+    packages=find_packages(),
     install_requires=open("requirements.txt").read().split(),
 )
