466ea756af3e87b0eeb0e1a8111696d2f5341fe8
Param Singh <param@metabrainz.org>
2018-02-22 14:47:29 +0530

    Create a method to init DebugToolbar in CustomFlask
    If Flask-Debug loads the toolbar before config has been
    loaded, it won't work. So we need a method that we can call
    after config has been loaded.

--- brainzutils/flask/test/test_main.py
+++ brainzutils/flask/test/test_main.py
@@ -1,9 +1,30 @@
 import unittest
-from brainzutils import flask
 
+from brainzutils import flask
 
 class FlaskTestCase(unittest.TestCase):
 
     def test_create_app(self):
         app = flask.CustomFlask(__name__)
         self.assertIsNotNone(app)
+
+    def test_debug_toolbar(self):
+        """ Tests that debug toolbar loads if initialized correctly
+        """
+
+        # create an app
+        app = flask.CustomFlask(__name__)
+        self.assertIsNotNone(app)
+        app.debug = True
+        app.config['SECRET_KEY'] = 'this is a totally secret key btw'
+        app.init_debug_toolbar()
+
+        # add a dummy route
+        @app.route('/')
+        def index():
+            return '<html><body>test</body></html>'
+
+        client = app.test_client()
+        response = client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('flDebug', str(response.data))
