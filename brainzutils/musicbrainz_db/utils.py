9271a6299ccc550d2ef19699fb662649768953db
spellew <spellew1298@gmail.com>
2019-07-13 12:11:06 -0400

    Refactored brainzutils.musicbrainz.utils.get_entities_by_gids to enable reuse

--- brainzutils/musicbrainz_db/utils.py
+++ brainzutils/musicbrainz_db/utils.py
@@ -31,6 +31,25 @@ REDIRECT_MODELS = {
 }
 
 
+def _filter_entity_model_by_gid(query, entity_type, mbids):
+    entity_model = ENTITY_MODELS[entity_type]
+    results = query.filter(entity_model.gid.in_(mbids)).all()
+    remaining_gids = list(set(mbids) - {entity.gid for entity in results})
+    entities = {str(entity.gid): entity for entity in results}
+    return entities, remaining_gids
+
+
+def _filter_redirect_model_by_gid(query, entity_type, entities, remaining_gids):
+    if remaining_gids:
+        redirect_model = REDIRECT_MODELS[entity_type]
+        query = query.add_entity(redirect_model).join(redirect_model)
+        results = query.filter(redirect_model.gid.in_(remaining_gids))
+        for entity, redirect_obj in results:
+            entities[redirect_obj.gid] = entity
+        remaining_gids = list(set(remaining_gids) - {redirect_obj.gid for entity, redirect_obj in results})
+    return entities, remaining_gids
+
+
 def get_entities_by_gids(query, entity_type, mbids):
     """Get entities using their MBIDs.
 
@@ -45,38 +64,26 @@ def get_entities_by_gids(query, entity_type, mbids):
         entity_type (str): Type of entity being queried.
         mbids (list): IDs of the target entities.
 
-    Returns:
+    Returns:d
         Dictionary of objects of target entities keyed by their MBID.
     """
-    entity_model = ENTITY_MODELS[entity_type]
-    results = query.filter(entity_model.gid.in_(mbids)).all()
-    remaining_gids = list(set(mbids) - {entity.gid for entity in results})
-    entities = {str(entity.gid): entity for entity in results}
-    if remaining_gids:
-        redirect_model = REDIRECT_MODELS[entity_type]
-        query = query.add_entity(redirect_model).join(redirect_model)
-        results = query.filter(redirect_model.gid.in_(remaining_gids))
-        for entity, redirect_obj in results:
-            entities[redirect_obj.gid] = entity
-        remaining_gids = list(set(remaining_gids) - {redirect_obj.gid for entity, redirect_obj in results})
+    entities, remaining_gids = _filter_entity_model_by_gid(query, entity_type, mbids)
+    entities, remaining_gids = _filter_redirect_model_by_gid(query, entity_type, entities, remaining_gids)
 
     if remaining_gids:
         raise mb_exceptions.NoDataFoundException("Couldn't find entities with IDs: {mbids}".format(mbids=remaining_gids))
-    
+
     return entities
 
 
 def get_entities_by_ids(query, entity_type, ids):
     """Get entities using their IDs.
-
     Note that the query may be modified before passing it to this
     function in order to save queries made to the database.
-
     Args:
         query (Query): SQLAlchemy Query object.
         entity_type (str): Type of entity being queried.
         ids (list): IDs of the target entities.
-
     Returns:
         Dictionary of objects of target entities keyed by their ID.
     """
