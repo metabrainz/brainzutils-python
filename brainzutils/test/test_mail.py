b23b717d610381039bcd8240c2295e6266725fa2
sjainit <sarthak2907@gmail.com>
2020-01-16 20:49:18 +0530

    Further changes

--- brainzutils/test/test_mail.py
+++ brainzutils/test/test_mail.py
@@ -10,35 +10,34 @@ from brainzutils import mail
 
 
 class MailTestCase(unittest.TestCase):
-    @mock.patch("smtplib.SMTP")
+
+    @mock.patch('smtplib.SMTP')
     def test_send_email(self, mock_smtp):
         app = flask.CustomFlask(__name__)
-        app.config["SMTP_SERVER"] = "localhost"
-        app.config["SMTP_PORT"] = 8080
+        app.config['SMTP_SERVER'] = 'localhost'
+        app.config['SMTP_PORT'] = 8080
 
         with app.app_context():
-            from_address = "noreply@metabrainz.org"
-            recipients = "sarthak2907@gmail.com"
-            text = "It is a test mail"
-            from_name = "ListenBrainz"
-            subject = "ListenBrainz Spotify Importer Error"
-            originalboundary = "===============2220963697271485568=="
+            from_address = 'noreply@metabrainz.org'
+            recipients = 'sarthak2907@gmail.com'
+            text = 'It is a test mail'
+            from_name = 'ListenBrainz'
+            subject = 'ListenBrainz Spotify Importer Error'
+            originalboundary = '===============2220963697271485568=='
             message = MIMEMultipart(boundary=originalboundary)
-            message["To"] = "<%s>" % (recipients)
-            message["Subject"] = subject
-            message["From"] = "%s <%s>" % (from_name, from_address)
-            message.attach(MIMEText(text, _charset="utf-8"))
+            message['To'] = '<%s>' % (recipients)
+            message['Subject'] = subject
+            message['From'] = '%s <%s>' % (from_name, from_address)
+            message.attach(MIMEText(text, _charset='utf-8'))
 
             mail.send_mail(
-                subject="ListenBrainz Spotify Importer Error",
-                text="It is a test mail",
-                recipients="sarthak2907@gmail.com",
+                subject='ListenBrainz Spotify Importer Error',
+                text='It is a test mail',
+                recipients='sarthak2907@gmail.com',
                 attachments=None,
-                from_name="ListenBrainz",
-                from_addr="noreply@metabrainz.org",
+                from_name='ListenBrainz',
+                from_addr='noreply@metabrainz.org',
                 boundary=originalboundary
             )
 
-            mock_smtp.return_value.sendmail.assert_called_once_with(
-                from_address, recipients, message.as_string()
-            )
+            mock_smtp.return_value.sendmail.assert_called_once_with(from_address, recipients, message.as_string())
