caab5f769314a4322c2ce4bd7425f4fe85b8cf30
sarthak_jain <sarthak2907@gmail.com>
2020-01-18 22:11:29 +0530

    Created test for mail and added 'To' Field (#38)
    * Test for mail and changed To field
    
    * Test for mail and changed To field
    
    * Changes in test_mail
    
    * Changes to test_mail file
    
    * Delete lastfailed
    
    * Fixed error in test file
    
    * Removed unnecessary parameters and imports
    
    * Changes to test_mail and mail files
    
    * Delete lastfailed
    
    * Formatting changes
    
    * Formatting changes
    
    * Further changes
    
    * Removed extra spacing
    
    * Updated mock and minor changes
    
    * Delete lastfailed

--- brainzutils/mail.py
+++ brainzutils/mail.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 """This module provides a way to send emails."""
+from __future__ import absolute_import
 from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from flask import current_app
+
 import smtplib
 import socket
 
-
 def send_mail(subject, text, recipients, attachments=None,
               from_name="MetaBrainz Notifications",
-              from_addr=None):
+              from_addr=None, boundary=None):
     """This function can be used as a foundation for sending email.
 
     Args:
@@ -26,14 +27,19 @@ def send_mail(subject, text, recipients, attachments=None,
         attachments = []
     if from_addr is None:
         from_addr = 'noreply@' + current_app.config['MAIL_FROM_DOMAIN']
-
+                     
     if current_app.config['TESTING']:  # Not sending any emails during the testing process
         return
 
     if not recipients:
         return
 
-    message = MIMEMultipart('mixed')
+    message =MIMEMultipart()
+
+    if boundary is not None:
+        message = MIMEMultipart(boundary=boundary)
+     
+    message['To']="<%s>" %(recipients)
     message['Subject'] = subject
     message['From'] = "%s <%s>" % (from_name, from_addr)
     message.attach(MIMEText(text, _charset='utf-8'))
@@ -44,7 +50,6 @@ def send_mail(subject, text, recipients, attachments=None,
         file_obj.close()  # FIXME(roman): This feels kind of hacky. Maybe there's a better way?
         attachment.add_header('content-disposition', 'attachment', filename=name)
         message.attach(attachment)
-
     try:
         smtp_server = smtplib.SMTP(current_app.config['SMTP_SERVER'], current_app.config['SMTP_PORT'])
     except (socket.error, smtplib.SMTPException) as e:
---
+++ brainzutils/test/test_mail.py
@@ -0,0 +1,42 @@
+import unittest
+import smtplib
+import mock as mock
+
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from brainzutils import flask
+from brainzutils import mail
+
+class MailTestCase(unittest.TestCase):
+
+    @mock.patch('smtplib.SMTP')
+    def test_send_email(self, mock_smtp):
+        app = flask.CustomFlask(__name__)
+        app.config['SMTP_SERVER'] = 'localhost'
+        app.config['SMTP_PORT'] = 8080
+
+        with app.app_context():
+            from_address = 'noreply@metabrainz.org'
+            recipients = 'sarthak2907@gmail.com'
+            text = 'It is a test mail'
+            from_name = 'ListenBrainz'
+            subject = 'ListenBrainz Spotify Importer Error'
+            boundary = '===============2220963697271485568=='
+            message = MIMEMultipart(boundary=boundary)
+            message['To'] = '<%s>' % (recipients)
+            message['Subject'] = subject
+            message['From'] = '%s <%s>' % (from_name, from_address)
+            message.attach(MIMEText(text, _charset='utf-8'))
+
+            mail.send_mail(
+                subject='ListenBrainz Spotify Importer Error',
+                text='It is a test mail',
+                recipients='sarthak2907@gmail.com',
+                attachments=None,
+                from_name='ListenBrainz',
+                from_addr='noreply@metabrainz.org',
+                boundary=boundary
+            )
+
+            mock_smtp.return_value.sendmail.assert_called_once_with(from_address, recipients, message.as_string())
