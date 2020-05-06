f0cbae2e12099efa27d8487cfdc62d6a875c88c5
Alastair Porter <alastair@metabrainz.org>
2020-05-06 14:41:10 +0200

    Use setuptools_scm to get the version from the git tag

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
-from brainzutils import __version__
 
 setup(
     name="brainzutils",
-    version=__version__,
     description="Python tools for MetaBrainz projects",
     author="MetaBrainz Foundation",
     author_email="support@metabrainz.org",
     py_modules=["brainzutils"],
     packages=find_packages(),
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
     install_requires=open("requirements.txt").read().split(),
 )
