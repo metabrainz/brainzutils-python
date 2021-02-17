3ea6d1aa606bdfb17cbdf91a37168dc40a62a00f
Kartik Ohri <kartikohri13@gmail.com>
2021-02-17 19:47:17 +0530

    Remove email logging handler

--- brainzutils/flask/loggers.py
+++ brainzutils/flask/loggers.py
@@ -20,31 +20,6 @@ def add_file_handler(app, filename, max_bytes=512 * 1024, backup_count=100):
     app.logger.addHandler(file_handler)
 
 
-def add_email_handler(app, mail_server, mail_port, mail_from_host,
-                      log_email_recipients, log_email_topic,
-                      level=logging.ERROR):
-    """Adds email notifications about captured logs."""
-    mail_handler = SMTPHandler(
-        (mail_server, mail_port),
-        "logs@" + mail_from_host,
-        log_email_recipients,
-        log_email_topic
-    )
-    mail_handler.setLevel(level)
-    mail_handler.setFormatter(logging.Formatter('''
-    Message type: %(levelname)s
-    Location: %(pathname)s:%(lineno)d
-    Module: %(module)s
-    Function: %(funcName)s
-    Time: %(asctime)s
-
-    Message:
-
-    %(message)s
-    '''))
-    app.logger.addHandler(mail_handler)
-
-
 def add_sentry(app, dsn, level=logging.WARNING, **options):
     """Adds Sentry logging.
 
