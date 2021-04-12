96cd24653fa167bfb40555c0c8bd965969e87ff3
Kartik Ohri <kartikohri13@gmail.com>
2021-04-12 21:18:45 +0530

    Enforce minimum python version

--- brainzutils/__init__.py
+++ brainzutils/__init__.py
@@ -1,3 +1,5 @@
+# TODO: We are the backport of importlib to support python 3.7.
+#  When we raise the minimum to python 3.8, we can remove this and use the builtin importlib module.
 from importlib_metadata import version, PackageNotFoundError
 
 try:
