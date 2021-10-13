1bef4d239755cd4751a2be77098aa1d119e80a72
Kartik Ohri <kartikohri13@gmail.com>
2021-10-13 16:06:48 +0530

    Set container name as sentry tag

--- brainzutils/flask/loggers.py
+++ brainzutils/flask/loggers.py
@@ -1,5 +1,6 @@
 # pylint: disable=invalid-name
 import logging
+import os
 from logging.handlers import RotatingFileHandler, SMTPHandler
 
 import sentry_sdk
@@ -35,3 +36,6 @@ def add_sentry(dsn, level=logging.WARNING, **options):
     sentry_sdk.init(dsn, integrations=[LoggingIntegration(level=level), FlaskIntegration(), RedisIntegration(),
                                        SqlalchemyIntegration()],
                     **options)
+    container_name = os.getenv("CONTAINER_NAME")
+    if container_name:
+        sentry_sdk.set_tag("container_name", container_name)
