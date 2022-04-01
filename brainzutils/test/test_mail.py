105c46f6651de1d820bc9911bf8726fdb99076e1
Alastair Porter <alastair@metabrainz.org>
2022-04-01 19:04:39 +0200

    Allow mail to be sent to multiple recipients
    Now the recipients argument must be a list, but everywhere
    that we use this it already was.
    
    Add some additional error checking to ensure that configuration is good
    before sending the message

--- brainzutils/test/test_mail.py
+++ brainzutils/test/test_mail.py
@@ -2,7 +2,6 @@ import unittest
 import smtplib
 from unittest import mock
 
-from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from brainzutils import flask
@@ -10,21 +9,51 @@ from brainzutils import mail
 
 class MailTestCase(unittest.TestCase):
 
+    def test_send_email_missing_config(self):
+        app = flask.CustomFlask(__name__)
+        with app.app_context():
+            with self.assertRaises(ValueError) as err:
+                mail.send_mail(
+                    subject='ListenBrainz Spotify Importer Error',
+                    text='It is a test mail',
+                    recipients=[],
+                    attachments=None,
+                    from_name='ListenBrainz',
+                    from_addr='noreply@metabrainz.org',
+                    boundary='b'
+                )
+            assert "Flask current_app requires config items" in str(err.exception)
+
+    def test_send_email_string_recipients(self):
+        app = flask.CustomFlask(__name__)
+        with app.app_context():
+            with self.assertRaises(ValueError) as err:
+                mail.send_mail(
+                    subject='ListenBrainz Spotify Importer Error',
+                    text='It is a test mail',
+                    recipients='wrongemail@metabrainz.org',
+                    attachments=None,
+                    from_name='ListenBrainz',
+                    from_addr='noreply@metabrainz.org',
+                    boundary='b'
+                )
+            assert str(err.exception) == "recipients must be a list of email addresses"
+
     @mock.patch('smtplib.SMTP')
     def test_send_email(self, mock_smtp):
         app = flask.CustomFlask(__name__)
         app.config['SMTP_SERVER'] = 'localhost'
-        app.config['SMTP_PORT'] = 8080
+        app.config['SMTP_PORT'] = 25
 
         with app.app_context():
             from_address = 'noreply@metabrainz.org'
-            recipients = 'sarthak2907@gmail.com'
+            recipients = ['musicbrainz@metabrainz.org', 'listenbrainz@metabrainz.org']
             text = 'It is a test mail'
             from_name = 'ListenBrainz'
             subject = 'ListenBrainz Spotify Importer Error'
             boundary = '===============2220963697271485568=='
             message = MIMEMultipart(boundary=boundary)
-            message['To'] = '<%s>' % (recipients)
+            message['To'] = "musicbrainz@metabrainz.org, listenbrainz@metabrainz.org"
             message['Subject'] = subject
             message['From'] = '%s <%s>' % (from_name, from_address)
             message.attach(MIMEText(text, _charset='utf-8'))
@@ -32,7 +61,7 @@ class MailTestCase(unittest.TestCase):
             mail.send_mail(
                 subject='ListenBrainz Spotify Importer Error',
                 text='It is a test mail',
-                recipients='sarthak2907@gmail.com',
+                recipients=recipients,
                 attachments=None,
                 from_name='ListenBrainz',
                 from_addr='noreply@metabrainz.org',
