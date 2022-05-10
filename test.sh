d67ef22403fd3f84ced9ca57eac81ff1de5ebf8f
Kartik Ohri <kartikohri13@gmail.com>
2022-05-10 18:05:19 +0530

    Remove containers after tests complete
    Leaving containers up after tests is annoying and consumes memory unnecessarily
    so remove those after tests have run. Also, Take care to exit with correct code
    after removing containers.

--- test.sh
+++ test.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 docker-compose -f test/docker-compose.yml -p brainzutils_test run --rm test "$@"
+RET=$?
+docker-compose -f test/docker-compose.yml -p brainzutils_test down
+exit $RET
