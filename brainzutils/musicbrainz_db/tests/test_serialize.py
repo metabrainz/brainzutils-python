b229b7bc2e034e84bd473b6dcb08fa3ec6261ef1
Kartik Ohri <kartikohri13@gmail.com>
2023-12-02 16:24:57 +0530

    Upgrade SQLAlchemy
    Remove sqlalchemy-dst, update SQLAlchemy.

--- brainzutils/musicbrainz_db/tests/test_serialize.py
+++ brainzutils/musicbrainz_db/tests/test_serialize.py
@@ -1,3 +1,5 @@
+from datetime import datetime, date
+
 from brainzutils.musicbrainz_db.serialize import serialize_recording, serialize_artist_credit, serialize_editor
 from brainzutils.musicbrainz_db.test_data import recording_numb_encore_explicit, artistcredit_jay_z_linkin_park, \
     editor_2
@@ -67,3 +69,22 @@ class SerializeTestCase(TestCase):
         editor = serialize_editor(editor_2)
         self.assertNotIn("password", editor)
         self.assertNotIn("ha1", editor)
+        self.assertEqual(editor, {
+            'id': 2324,
+            'name': 'Editor 2',
+            'privs': 3,
+            'email': 'editor@example.com',
+            'website': 'example.com',
+            'bio': 'Random\neditor',
+            'member_since': datetime(2014, 12, 1, 14, 6, 42, 321443),
+            'email_confirm_date': datetime(2014, 12, 1, 14, 6, 42, 321443),
+            'last_login_date': datetime(2014, 12, 1, 14, 6, 42, 321443),
+            'last_updated': datetime(2014, 12, 1, 14, 6, 42, 321443),
+            'birth_date': date(1999, 1, 1),
+            'deleted': False,
+            'gender': None,
+            'area': {
+                "mbid": "4479c385-74d8-4a2b-bdab-f48d1e6969ba",
+                "name": "Hämeenlinna"
+            }
+        })
