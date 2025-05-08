271c17935b497e05d65a9a070697c335bf585696
Kartik Ohri <kartikohri13@gmail.com>
2025-05-08 19:29:04 +0530

    Upgrade dependencies and drop Python versions < 3.10 (#103)
    Updated the project to require Python 3.10 or newer, removing compatibility for older versions. Upgraded all dependencies to their latest versions and removed unused ones like Flask-UUID. Cleaned up Docker-related files by removing unused configurations and updated Python versioning.

--- brainzutils/flask/__init__.py
+++ brainzutils/flask/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from flask_uuid import FlaskUUID
 from flask_debugtoolbar import DebugToolbarExtension
 
 
@@ -7,7 +6,6 @@ class CustomFlask(Flask):
     """Custom version of Flask with our bells and whistles."""
 
     def __init__(self, import_name, config_file=None, debug=None,
-                 use_flask_uuid=False,
                  *args, **kwargs):
         """Create an instance of Flask app.
 
@@ -18,15 +16,12 @@ class CustomFlask(Flask):
                 config_file (str): Path to a config file that needs to be loaded.
                     Should be in a form of Python module.
                 debug (bool): Override debug value.
-                use_flask_uuid (bool): Turn on Flask-UUID extension if set to True.
         """
         super(CustomFlask, self).__init__(import_name, *args, **kwargs)
         if config_file:
             self.config.from_pyfile(config_file)
         if debug is not None:
             self.debug = debug
-        if use_flask_uuid:
-            FlaskUUID(self)
 
     def init_debug_toolbar(self):
         """This method initializes the Flask-Debug extension toolbar for the
